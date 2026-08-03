2161d1c096b2a6841399ee19cb4ec28747b79372
Swapna <banaganiswapna77@gmail.com>
2026-08-03 19:14:58 +0530

    Learning Commitment

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/Learing-Commitment.java
@@ -0,0 +1,8 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Track: Java Development");
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
