e7ac10aa9dd8075688fa663767f750be90bf0f13
Swapna <banaganiswapna77@gmail.com>
2026-08-03 19:48:44 +0530

    student id card

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/StudentIdCard.java
@@ -0,0 +1,12 @@
+
+public class StudentIdCard {
+
+    public static void main(String[] args) {
+        System.out.println("--------------------");
+        System.out.println("STUDENT PROFILE");
+        System.out.println("Name: Alex");
+        System.out.println("Course: Java");
+        System.out.println("Level: Beginner");
+        System.out.println("--------------------");
+    }
+}
