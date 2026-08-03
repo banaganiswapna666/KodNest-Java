a4c3cff8aa75159f6cbc090433fb52256fd5e07c
Swapna <banaganiswapna77@gmail.com>
2026-08-03 15:51:21 +0530

    firstjavaprogram

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/M02-Introduction to-java/T01-/Main.java
@@ -0,0 +1,12 @@
+
+class Main {
+
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = 20;
+        float e = 34.5f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(e);
+    }
+}
