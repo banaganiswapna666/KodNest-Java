35ae7360388072e91a8df14ef99c63c044802018
Swapna <banaganiswapna77@gmail.com>
2026-08-04 11:31:59 +0530

    Days

---
+++ track/M02/T01/ST01/P1/SimpleInterest.java
@@ -0,0 +1,19 @@
+
+public class SimpleInterest {
+
+    public static void main(String[] args) {
+        double principle = 1000.0;
+        double rate = 6.5;
+        double time = 2.0;
+        double weight = 72.0;
+        double height = 1.8;
+        int mark1 = 78;
+        int mark2 = 84;
+        int mark3 = 69;
+        int mark4 = 91;
+        int mark5 = 88;
+        int totalMarks = mark1 + mark2 + mark3 + mark4 + mark5;
+        double simpleInterest = (principle *)
+    }
+}
+}
