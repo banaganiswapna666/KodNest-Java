51d38f583472d97239ad0be63fbfa11c82e7d63f
Swapna <banaganiswapna77@gmail.com>
2026-08-04 12:30:15 +0530

    Main

---
+++ track/JavaPracticeTrack/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num = -34;
+        if (num > 0) {
+            System.out.println("positive");
+        } else if (num < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+}
