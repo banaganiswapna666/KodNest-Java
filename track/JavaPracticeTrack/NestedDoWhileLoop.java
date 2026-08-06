d8b264be11b8227c597f842332c3bbe0fc7d8af3
Swapna <banaganiswapna77@gmail.com>
2026-08-06 11:47:43 +0530

    Nested For Loop

---
+++ track/JavaPracticeTrack/NestedDoWhileLoop.java
@@ -0,0 +1,16 @@
+
+class NestedDoWhileLoop {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 5);
+        int j = 1;
+        do {
+            System.out.println(j);
+            j++;
+        } while (j <= 5);
+    }
+}
