170ae0f397d153e15e66201e37d7285c2aa7f9aa
Swapna <banaganiswapna77@gmail.com>
2026-08-06 13:00:23 +0530

    Break Outer loop

---
+++ track/JavaPracticeTrack/Program.java
@@ -0,0 +1,16 @@
+
+class Program {
+
+    public static void main(String[] args) {
+        Outer:
+        for (int i = 1; i <= 4; i++) {
+            Inner:
+            for (int j = 1; j <= 4; j++) {
+                if (j == 4) {
+                    break Outer;
+                }
+                System.out.println("i:" + i + "j:" + j);
+            }
+        }
+    }
+}
