06eb214b8746ba66d3698d29bf8311716c7d987b
Swapna <banaganiswapna77@gmail.com>
2026-08-07 12:05:38 +0530

    CharAt

---
+++ track/JavaPracticeTrack/CharAt.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+class CharAt {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter char:");
+        char ch = scan.next().charAt(2);
+        System.out.println(ch);
+    }
+}
