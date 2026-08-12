2863bf0e45829b7aac2fe71e9f6a565b97ab6879
Swapna <banaganiswapna77@gmail.com>
2026-08-12 13:06:52 +0530

    Method overloading

---
+++ track/JavaPracticeTrack/MethodOverLoading.java
@@ -0,0 +1,45 @@
+
+class Addition {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    double add(int a, double b) {
+        return a + b;
+    }
+
+    double add(double a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    double add(int a, int b, double c) {
+        return a + b + c;
+    }
+}
+
+class MethodOverLoading {
+
+    public static void main(String[] args) {
+        Addition a1 = new Addition();
+        System.out.println(a1.add(10, 20));
+        System.out.println(a1.add(2.0, 6.0));
+        System.out.println(a1.add(10, 5.5));
+        System.out.println(a1.add(10, 20, 30));
+        System.out.println(a1.add(2.0, 3.0, 6.0));
+        System.out.println(a1.add(10, 20, 5.5));
+        System.out.println(a1.add(8.0, 10));
+    }
+}
--- track/JavaPracticeTrack/StudentDetails.java
+++ track/JavaPracticeTrack/StudentDetails.java
@@ -1,22 +1,28 @@
-class Student{
+
+class Student {
+
     String name;
     int age;
     double height;
-    void input(String name,int age,double height){
-        name = name;
-        age = age;
-        height = height;
+
+    void input(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
     }
-    void display(){
+
+    void display() {
         System.out.println(name);
         System.out.println(age);
         System.out.println(height);
     }
 }
+
 class StudentDetails {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Student s1 = new Student();
-        s1.input("swapna",18,6.0);
+        s1.input("swapna", 18, 6.0);
         s1.display();
     }
 }
