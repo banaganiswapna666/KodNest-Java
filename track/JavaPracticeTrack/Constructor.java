1c0ff940f3bb482996cc840b1b528708e1cc3354
Swapna <banaganiswapna77@gmail.com>
2026-08-14 11:45:17 +0530

    Constructor

---
+++ track/JavaPracticeTrack/Constructor.java
@@ -0,0 +1,27 @@
+
+class Student {
+
+    String name;
+    int age;
+    double height;
+
+    Student(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
+
+class Constructor {
+
+    public static void main(String[] args) {
+        Student s1 = new Student("swapna", 21, 5.5);
+        s1.display();
+    }
+}
