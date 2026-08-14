b1b8d173a2969db471b5dce2a9089472020412f2
Swapna <banaganiswapna77@gmail.com>
2026-08-14 22:25:28 +0530

    Overloading Constructor

---
+++ track/M02/OverloadedConstructor.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+class FoodOrder {
+
+    String item;
+    int quantity;
+
+    // Constructor 1: Default quantity is 1
+    FoodOrder(String item) {
+        this.item = item;
+        this.quantity = 1;
+    }
+
+    // Constructor 2: User provides quantity
+    FoodOrder(String item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    void display() {
+        System.out.println("\n--- Food Order Details ---");
+        System.out.println("Food Item: " + item);
+        System.out.println("Quantity: " + quantity);
+    }
+}
+
+public class OverloadedConstructor {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String item = scanner.nextLine();
+        int quantity = scanner.nextInt();
+
+        FoodOrder f1 = new FoodOrder(item);
+        FoodOrder f2 = new FoodOrder(item, quantity);
+        f1.display();
+        f2.display();
+    }
+}
