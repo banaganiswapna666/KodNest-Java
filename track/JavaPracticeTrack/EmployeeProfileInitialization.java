e1194ef9c4db64f572cf7a0a051bd135be21843e
Swapna <banaganiswapna77@gmail.com>
2026-08-14 23:51:45 +0530

    Employee Profile Initialization

---
+++ track/JavaPracticeTrack/EmployeeProfileInitialization.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+class Employee {
+
+    int id;
+    String name;
+    String department;
+
+    Employee(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    void display() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("Department: " + department);
+    }
+}
+
+public class EmployeeProfileInitialization {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        String name = scanner.nextLine();
+        String department = scanner.nextLine();
+
+        Employee e = new Employee(id, name, department);
+        e.display();
+
+        scanner.close();
+    }
+}
