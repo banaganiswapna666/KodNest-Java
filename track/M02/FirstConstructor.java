cc73223e99c5d22fd726a208aaa902469a4b3d6d
Swapna <banaganiswapna77@gmail.com>
2026-08-14 12:42:41 +0530

    First Constructor

---
+++ track/M02/FirstConstructor.java
@@ -0,0 +1,17 @@
+
+class Robot {
+
+    // Constructor
+    Robot() {
+        System.out.println("Beep beep! Robot reporting for Java duty!");
+    }
+}
+
+public class FirstConstructor {
+
+    public static void main(String[] args) {
+
+        // Create a Robot object
+        Robot r = new Robot();
+    }
+}
