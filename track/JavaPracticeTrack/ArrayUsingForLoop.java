64f4a6ca46120f8d583929e246db1f7d30edd564
Swapna <banaganiswapna77@gmail.com>
2026-08-17 12:26:14 +0530

    Arrays Using For Loop

---
+++ track/JavaPracticeTrack/ArrayUsingForLoop.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+class ArrayUsingForLoop {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a[] = new int[5];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i <= 4; i++) {
+            a[i] = scan.nextInt();
+
+        }
+        System.out.println("Array elements are:");
+        for (int i = 0; i <= a.length - 1; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
