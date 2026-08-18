e0a7f8392a6457a46fc82001e9b5c710ae4cd248
Swapna <banaganiswapna77@gmail.com>
2026-08-18 11:30:56 +0530

    Max Array Element

---
+++ track/JavaPracticeTrack/MaxArrayElement.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+class MaxArrayElement {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int arr[] = new int[5];
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i <= arr.length - 1; i++) {
+            arr[i] = scan.nextInt();
+        }
+        System.out.println("Array Elements are:");
+        for (int i = 0; i <= arr.length - 1; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        int max = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Max Element is: " + max);
+    }
+}
