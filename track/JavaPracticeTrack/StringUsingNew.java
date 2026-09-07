60720838d3b171b96d27193e5f6e1358666fb076
Swapna <banaganiswapna77@gmail.com>
2026-09-07 17:25:02 +0530

    StringWithoutUsingNew

---
+++ track/JavaPracticeTrack/StringUsingNew.java
@@ -0,0 +1,13 @@
+
+class StringUsingNew {
+
+    public static void main(String[] args) {
+        String s1 = new String("Java");
+        String s2 = new String("Java");
+        if (s1.equals(s2)) {
+            System.out.println("Ref are equal");
+        } else {
+            System.out.println("Ref are not equal");
+        }
+    }
+}
